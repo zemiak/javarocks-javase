fac75883b391d242fe16014e710b7555cc2b3c48
zemiak <zemiak@gmail.com>
2019-04-28 11:35:30 +0200

    Commons dependency problem logged

--- src/main/java/com/zemiak/CommonsExample.java
+++ src/main/java/com/zemiak/CommonsExample.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class CommonsExample {
     public void stringUtilsExample() {
-        //StringUtils
+        StringUtils.isEmpty("Hello");
     }
     
 }
